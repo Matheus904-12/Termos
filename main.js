31a174709ce5aab3c5b064fe0293b18d442115b9
Matheus904-12 <67837282+Matheus904-12@users.noreply.github.com>
2023-10-30 17:03:11 -0300

    Terminado

--- main.js
+++ main.js
@@ -1,163 +1,170 @@
-const divisao = document.querySelector(".divisao");
-const apagaEnter = document.querySelector("#apagar-e-enter");
-const primeiraLinha = document.querySelector("#primeira-linha");
-const segundaLinha = document.querySelector("#segunda-linha");
-const terceiraLinha = document.querySelector("#terceira-linha");
+//Selecionando os elementos HTML
+const divisao = document.querySelector(".divisao")
+const apagaEnter = document.querySelector("#apagar-e-enter")
+const primeiraLinha = document.querySelector("#primeira-linha")
+const segundaLinha = document.querySelector("#segunda-linha")
+const terceiraLinha = document.querySelector("#terceira-linha")
 
-const teclaPrimeiraLinha = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'];
-const teclaSegundaLinha = ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'];
-const teclaTerceiraLinha = ['Z', 'X', 'C', 'V', 'B', 'N', 'M'];
 
-const linha = 6;
-const coluna = 5;
-let linhaAtual = 0;
-let colunaAtual = 0;
+//Definindo as teclas
+const teclaPrimeiraLinha = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P']
+const teclaSegundaLinha = ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L']
+const teclaTerceiraLinha = ['Z', 'X', 'C', 'V', 'B', 'N', 'M']
 
-let palavras = ['SENAI', 'NOITE', 'MILHO', 'LETRA', 'MOUSE'];
+//Definindo as variáveis do jogo
+const linha = 6
+const coluna = 5
+let linhaAtual = 0
+let colunaAtual = 0
 
-let palavra = palavras[Math.floor(Math.random() * palavras.length)];
+//Definindo o array de palavras
+let palavras = ['SENAI', 'NOITE', 'MILHO', 'LETRA', 'MOUSE']
 
-let palavraMapa = {};
-for (let i = 0; i < palavra.length; i++) {
-    palavraMapa[palavra[i]] = i;
+//Seleciona uma palavra aleatória dentro do array palavras e guarda na variável palavra
+let palavra = palavras[Math.floor(Math.random() * palavras.length)]
+let palavraMapa = {}
+for (let i = 0; i < palavras.length; i++) {
+    //separa as letras da palavra
+    palavraMapa[palavras[i] = i]//separa cada letra em uma posição do palavraLinha -- palavraMapa ['S', 'E', 'A', 'N','I'],
 }
 
-const tentativas = [];
+const tentativas = []
 
+//criando a grade de caixas texto
 for (let linhaIndex = 0; linhaIndex < linha; linhaIndex++) {
-    tentativas[linhaIndex] = new Array(coluna);
-
-    const divisaoLinha = document.createElement('div');
-
-    divisaoLinha.setAttribute('id', 'linha' + linhaIndex);
-
-    divisaoLinha.setAttribute('class', 'div-linha');
-
+    //vai montar as linhas
+    tentativas[linhaIndex] = new Array(coluna)
+    const divisaoLinha = document.createElement('div')  //cria um novo array com o numero total de colunas
+    divisaoLinha.setAttribute('id', 'linha' + linhaIndex) //cria uma nova dic
+    divisaoLinha.setAttribute('class', 'div-linha')    //define o atributo ID
     for (let colunaIndex = 0; colunaIndex < coluna; colunaIndex++) {
-        const divisaoColuna = document.createElement('div');
-        divisaoColuna.setAttribute('id', 'linha' + linhaIndex + 'coluna' + colunaIndex);
+        //vai mostrar as colunas
+        const divisaoColuna = document.createElement('div')
+        divisaoColuna.setAttribute('id', 'linha' + linhaIndex + 'coluna' + colunaIndex)
         let classColuna;
         if (linhaIndex === 0) {
-            classColuna = 'div-coluna digitando';
+            classColuna = 'div-coluna digitando'
         } else {
-            classColuna = 'div-coluna desativado';
+            classColuna = 'div-coluna desativado'
         }
-        divisaoColuna.setAttribute('class', classColuna);
-        divisaoLinha.append(divisaoColuna);
-
-        tentativas[linhaIndex][colunaIndex] = '';
+        divisaoColuna.setAttribute('class', classColuna)
+        divisaoLinha.append(divisaoColuna)  //adiciona a coluna como filho da linha
+        tentativas[linhaIndex][colunaIndex] = '' //a tentativa começa vazia
     }
-    divisao.append(divisaoLinha);
+    divisao.append(divisaoLinha)//adiciona a linha como filha da divisao
 }
 
 const checkTentativa = () => {
-    const tentativa = tentativas[linhaAtual].join('');
-
-    if (tentativa.length !== coluna) {
-        return;
+    const tentativa = tentativas[linhaAtual].join('') //cria um objeto a partir do array 'tentativas' usando o metodo join
+    if (tentativa.length !== coluna) { //(!==)diferente
+        //verifica se já foi colocando uma letra (tentativa) na coluna
+        return
     }
 
-    let atColuna = document.querySelectorAll('.digitando');
-
+    let atColuna = document.querySelectorAll('.digitando')
     for (let i = 0; i < coluna; i++) {
-        const letra = tentativa[i];
+        const letra = tentativa[i] //seleciona a letra corresponde a coluna atual
 
         if (palavraMapa[letra] === undefined) {
-            atColuna[i].classList.add('errado');
+            //verifica se letra atual não existe no palavraMapa
+            atColuna[i].classList.add('errado')
         } else {
             if (palavraMapa[letra] === i) {
-                atColuna[i].classList.add('certo');
+                atColuna[i].classList.add('certo')
             } else {
-                atColuna[i].classList.add('deslocada');
+                atColuna[i].classList.add('deslocada')
             }
         }
     }
-
-    if (tentativa === palavra) {
-        window.alert('Parabens, Voce conseguiu!');
-        return;
-    } else {
-        if (linhaAtual === linha - 1) {
-            window.alert('erro!');
-        } else {
-            proximaLinha();
+    if(tentativa === palavra){
+        window.alert('Parabéns, você conseguiu!')
+        return
+    }else{
+        if(linhaAtual === linha-1){
+            //verifica se todas alinhas já foram
+            window.alert( 'Errou:')
+        }else{
+            proximaLinha()
         }
     }
-};
+}
 
-const proximaLinha = () => {
-    let digColuna = document.querySelectorAll('.digitando');
 
-    for (let i = 0; i < digColuna.length; i++) {
-        digColuna[i].classList.remove('digitando');
-        digColuna[i].classList.add('desativado');
+const proximaLinha = () =>{
+    let digColuna = document.querySelectorAll('.digitando')
+    //seleciona todos os elementios com a classe digitando
+    for(let i = 0; i< digColuna.length; i++){
+        digColuna[i].classList.remove('digitando')
+        digColuna[i].classList.add('desativado')
     }
+    linhaAtual++
+    colunaAtual = 0
+    //linhaAtual++ para ir para a proxima linha e a coluna volta a ser 0 para ser a primeira caixinha da linha
 
-    linhaAtual++;
-    colunaAtual = 0;
-
-    const linhaAtualElemento = document.querySelector('#linha' + linhaAtual);
-    let atColuna = linhaAtualElemento.querySelectorAll('.div-coluna');
-
-    for (let i = 0; i < atColuna.length; i++) {
-        atColuna[i].classList.remove('desativado');
-        atColuna[i].classList.remove('digitando');
+    const linhaAtualElemento = document.querySelector('#linha'+linhaAtual)
+    let atColuna = linhaAtualElemento.querySelectorAll('.div-coluna')
+    for(let i = 0; i< atColuna.length; i++){
+        atColuna[i].classList.remove('desativado')
+        atColuna[i].classList.add('digitando')
     }
-};
+}
 
 const tecladoOnClick = key => {
-    if (colunaAtual === coluna) {
-        return;
+    if(colunaAtual === coluna){
+        //verifica se acabou as colunas
+        return
     }
 
-    const divAtual = document.querySelector('#linha' + linhaAtual + 'coluna' + colunaAtual);
-    divAtual.textContent = key;
-    tentativas[linhaAtual][colunaAtual] = key;
-    colunaAtual++;
-};
+    const divAtual = document.querySelector('#linha'+linhaAtual+'coluna'+colunaAtual)
+    divAtual.textContent = key // o conteudo do texto será igual a tecla digitada
+
+    tentativas[linhaAtual][colunaAtual]=key
+    colunaAtual++
+}
 
 const criarLinhaTeclado = (keys, linhaTeclado) => {
-    keys.forEach(key => {
-        let botaoElemento = document.createElement('button');
-        botaoElemento.textContent = key;
-        botaoElemento.setAttribute('id', key);
-        botaoElemento.addEventListener('click', () => tecladoOnClick(key));
-        linhaTeclado.append(botaoElemento);
-    });
-};
+    keys.forEach(key =>{
+        //vai ler todas as teclas
+        let botaoElemento = document.createElement('button')//vai criar os botões
+        botaoElemento.textContent = key
+        botaoElemento.setAttribute('id', key)
+        botaoElemento.addEventListener('click', () => tecladoOnClick(key))
+        linhaTeclado.append(botaoElemento)
+    })
+}
 
-criarLinhaTeclado(teclaPrimeiraLinha, primeiraLinha);
-criarLinhaTeclado(teclaSegundaLinha, segundaLinha);
-criarLinhaTeclado(teclaTerceiraLinha, terceiraLinha);
+criarLinhaTeclado(teclaPrimeiraLinha, primeiraLinha)
+criarLinhaTeclado(teclaSegundaLinha, segundaLinha)
+criarLinhaTeclado(teclaTerceiraLinha, terceiraLinha)
 
 const backspace = () => {
-    if (colunaAtual === 0) {
-        return;
+    if(colunaAtual === 0){
+        return
     }
+    colunaAtual--
+    tentativas[linhaAtual][colunaAtual] = ''//o quadrado volta a ficar vazio
+    const div = document.querySelector('#linha'+linhaAtual+'coluna'+colunaAtual)
+    div.textContent = ''
+}
 
-    colunaAtual--;
-    tentativas[linhaAtual][colunaAtual] = '';
-    const div = document.querySelector('#linha' + linhaAtual + 'coluna' + colunaAtual);
-    div.textContent = '';
-};
+//criar p botao apagar e enter
+const backspaceBotao = document.createElement('button')
+backspaceBotao.addEventListener('click', backspace)
+backspaceBotao.textContent = '<x'
+apagaEnter.append(backspaceBotao)
 
-const backspaceBotao = document.createElement('button');
-backspaceBotao.addEventListener('click', backspace);
-backspaceBotao.textContent = '<x';
-apagaEnter.append(backspaceBotao);
+const enterBotao = document.createElement('button')
+enterBotao.addEventListener('click', checkTentativa)
+enterBotao.textContent = 'ENTER'
+apagaEnter.append(enterBotao)
 
-const enterBotao = document.createElement('button');
-enterBotao.addEventListener('click', checkTentativa);
-enterBotao.textContent = 'ENTER';
-apagaEnter.append(enterBotao);
-
-document.onkeydown = function (e) {
-    e = e || window.event;
-    if (e.key === 'Enter') {
-        checkTentativa();
-    } else if (e.key === 'Backspace') {
-        backspace();
-    } else {
-        tecladoOnClick(e.key.toUpperCase());
+document.onkeydown = function(evt){
+    evt = evt || window.evt
+    if(evt.key === 'Enter'){
+        checkTentativa()
+    }else if (evt.key === 'Backspace'){
+        backspace()
+    }else{
+        tecladoOnClick(evt.key.toLocaleLowerCase())
     }
-};
+}
